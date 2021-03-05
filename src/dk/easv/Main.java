793855bb4237886eaf3fc3581390646cf2ce9931
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2021-03-05 13:23:47 +0100

    Refactoring

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -2,12 +2,30 @@ package dk.easv;
 
 import dk.easv.bll.NavigationSystem;
 import dk.easv.bll.TransportMethod;
+import dk.easv.model.Route;
 
 public class Main {
 
     public static void main(String[] args) {
+        String from = "Esbjerg City";
+        String to = "Business Academy South West";
+
         NavigationSystem nav = new NavigationSystem(TransportMethod.Bus);
-        for(String waypoint : nav.findRoute("Esbjerg City", "Business Academy South West").getWaypoints()) {
+        Route busRoute = nav.findRoute(from, to);
+
+        nav.setTransportMethod(TransportMethod.Car);
+        Route carRoute = nav.findRoute(from, to);
+
+        nav.setTransportMethod(TransportMethod.Walking);
+        Route walkingRoute = nav.findRoute(from, to);
+
+        printRoute(busRoute);
+        printRoute(carRoute);
+        printRoute(walkingRoute);
+    }
+
+    private static void printRoute(Route route) {
+        for(String waypoint : route.getWaypoints()) {
             System.out.println(waypoint);
         }
     }
--- src/dk/easv/bll/NavigationSystem.java
+++ src/dk/easv/bll/NavigationSystem.java
@@ -12,20 +12,35 @@ public class NavigationSystem {
 
     public Route findRoute(String startingPoint, String destinationPoint) {
         switch (transportMethod) {
-
             case Walking -> {
-                return new Route( startingPoint, "Skolegade", "Kirkegade", destinationPoint);
+                return findWalkingRoute(startingPoint, destinationPoint);
             }
             case Car -> {
-                return new Route(startingPoint, "Skolegade", "Stormgade", destinationPoint);
+                return findCarRoute(startingPoint, destinationPoint);
             }
             case Bus -> {
-                return new Route(startingPoint, "Havnegade", "Strandbygade", "Gl. Vardevej", destinationPoint);
+                return findBusRoute(startingPoint, destinationPoint);
             }
             default -> {
                 return new Route();
             }
         }
+    }
+
+    private Route findCarRoute(String startingPoint, String destinationPoint) {
+        return new Route(startingPoint, "Skolegade", "Stormgade", destinationPoint);
+    }
+
+    private Route findBusRoute(String startingPoint, String destinationPoint) {
+        return new Route(startingPoint, "Havnegade", "Strandbygade", "Gl. Vardevej", destinationPoint);
+    }
+
+    private Route findWalkingRoute(String startingPoint, String destinationPoint) {
+        return new Route( startingPoint, "Skolegade", "Kirkegade", destinationPoint);
+    }
+
+    public void setTransportMethod(TransportMethod method) {
+        transportMethod = method;
     }
 
 }
